7c73939f7b979693e167ce73cef9bb53eff3e9e3
Connor Birkhold <connorbirkhold@gmail.com>
2022-05-23 17:29:37 +0000

    daily view created

---
+++ models/stocks_daily_view.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized="view",
+        sort = "date"
+
+    )
+}}
+
+
+SELECT  
+    A.date
+    , A.ticker
+    , A.open
+    , A.close
+    , CASE WHEN B.action = 'dividends' THEN B.value END AS dividends
+    , CASE WHEN B.action = 'stock_splits' THEN B.value END AS stock_splits
+FROM {{ ref('stocks_daily_prices') }} A 
+LEFT JOIN {{ ref('stocks_daily_actions') }} B 
+ON A.date = B.date
+AND A.ticker = B.ticker
