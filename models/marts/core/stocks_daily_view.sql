d420586cb20ffc20688f1e4b93cb420fe356ea83
Connor Birkhold <connorbirkhold@gmail.com>
2022-05-25 14:17:58 +0000

    made stocks_daily_view into partitioned table

--- models/marts/core/stocks_daily_view.sql
+++ models/marts/core/stocks_daily_view.sql
@@ -1,7 +1,11 @@
 {{
     config(
-        materialized="view",
-        sort = "date"
+        materialized = "table",
+        partition_by = {
+            "field": "date",
+            "data_type": "timestamp",
+            "granularity": "day"
+        }
 
     )
 }}
